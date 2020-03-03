9b394b0f75e77213cd04d3ee892c5c33d5d49a7d
Maria <shrizzleshreet101@yahoo.com>
2020-03-03 13:41:05 -0500

    added css color vars + typefaces

--- frontend/src/routes/Components/QuaternaryButton.js
+++ frontend/src/routes/Components/QuaternaryButton.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 class QuaternaryButton extends Component {
-  render() {
-    const quaternary={
-      color:"#1A1A1A",
-      borderRadius: "0.625rem",
-      border: "0.0625rem solid #0364FF",
-      float:"right",
-      minWidth: "8rem",
-  };
+    render() {
+        const quaternary = {
+            color: "var(--toast-black)",
+            borderRadius: "0.625rem",
+            float: "right",
+            minWidth: "8rem",
+        };
 
-    return (
-      <button className="btn" style={quaternary} type={this.props.type} onClick={this.props.handleClick}>{this.props.label}</button>
-    );
-  }
+        return ( <button className = "btn"
+            style = { quaternary }
+            type = { this.props.type }
+            onClick = { this.props.handleClick } > { this.props.label } </button>
+        );
+    }
 }
 
 export default QuaternaryButton;
