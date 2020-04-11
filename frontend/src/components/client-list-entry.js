c87460467e241685d2b417feba7a27e455a228bc
Maria <shrizzleshreet101@yahoo.com>
2020-04-11 13:41:49 -0400

    [#85] - modal menu component + integrate

--- frontend/src/components/client-list-entry.js
+++ frontend/src/components/client-list-entry.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Style from "style-it";
 
+import ToastIcon from "./toast/toast-icon";
+import ToastMenu from "./toast/toast-menu";
+
 class ClientListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.menuLabels = ["Profile", "Plan", "Action Items"];
+    this.menuIconNames = ["user", "barchart", "checkcircle"];
+    this.menuLinks = ["profile", "plan", "actionitems"];
+  }
   render() {
     const styles = `
     .entry {
-        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.75rem 1.75rem;
         border-radius: 0.625rem;
-        margin: 0.3rem;
+        margin-bottom: 0.5rem;
+        height: 1.75rem;
     }
     
     .entry:nth-child(odd) {
@@ -17,12 +30,54 @@ class ClientListEntry extends React.Component {
     .entry:nth-child(even) {
         background-color: var(--toast-neutral-5);
     }
+
+    .link {
+      font-weight: bold;
+    }
+
+    .link:hover {
+      color: var(--toast-blue-1);
+    }
+
+    .ellipsis {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      padding: 0.25rem;
+      cursor: pointer;
+    }
+
+    .entry:nth-child(odd) .ellipsis:hover {
+      background: var(--toast-neutral-5);
+    }
+
+    .entry:nth-child(even) .ellipsis:hover {
+      background: var(--toast-neutral-4);
+    }
     `;
 
     return Style.it(
       `${styles}`,
       <div className="entry">
-        <a href="/profile">{this.props.clientName}</a>
+        <a href="/profile" className="link">
+          {this.props.firstName} {this.props.middleName} {this.props.lastName}
+        </a>
+
+        <ToastMenu
+          labels={this.menuLabels}
+          iconNames={this.menuIconNames}
+          links={this.menuLinks}
+        >
+          <div className="ellipsis">
+            <ToastIcon
+              name="ellipsis"
+              width={24}
+              height={24}
+              fill="var(--toast-neutral-1)"
+            />
+          </div>
+        </ToastMenu>
       </div>
     );
   }
--- frontend/src/components/page_content/clients-content.js
+++ frontend/src/components/page_content/clients-content.js
@@ -1,14 +1,63 @@
 import React from "react";
+import Style from "style-it";
 
 import ClientListEntry from "../client-list-entry";
 
 class ClientsContent extends React.Component {
   render() {
-    return (
-      <React.Fragment>
-        <ClientListEntry clientName="John Smith" />
-        <ClientListEntry clientName="Blah Blah" />
-      </React.Fragment>
+    const styles = `
+    .container {
+      height: calc(90vh - 5.5rem);
+      overflow: scroll;
+    }
+    `;
+
+    return Style.it(
+      `${styles}`,
+      <div className="container">
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+        <ClientListEntry
+          firstName="John"
+          middleName="Joseph"
+          lastName="Smith"
+        />
+        <ClientListEntry firstName="Sarah" middleName="" lastName="Porter" />
+      </div>
     );
   }
 }
