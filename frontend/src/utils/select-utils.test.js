b9342785b1b10d324203a74b41e2e0084620a51d
Sita Robinson <sita.robinson@gmail.com>
2021-02-28 09:26:51 -0500

    Fixing tests

--- frontend/src/utils/select-utils.test.js
+++ frontend/src/utils/select-utils.test.js
@@ -14,10 +14,10 @@ test("getStateCode - not found", () => {
 
 test("getBirthYearOptions - default maxOffset", () => {
   expect(getBirthYearOptions()[0]).toBe(new Date().getFullYear());
-  expect(getBirthYearOptions()[120]).toBe(1900);
+  expect(getBirthYearOptions()[120]).toBe(new Date().getFullYear() - 120);
 });
 
 test("getBirthYearOptions - custom maxOffset", () => {
   expect(getBirthYearOptions(50)[0]).toBe(new Date().getFullYear());
-  expect(getBirthYearOptions(50)[50]).toBe(1970);
+  expect(getBirthYearOptions(50)[50]).toBe(new Date().getFullYear() - 50);
 });
